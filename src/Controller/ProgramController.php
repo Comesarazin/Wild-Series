e769caa0de0774e3cf1ab4ac020a6f08291a9b44
ComeSarazin <sasacoco@orange.fr>
2023-11-30 11:50:14 +0100

    05 - Symfony : Le routing avancé fini

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,11 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/program', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
+    }
+
+    #[Route('/program/{id}', methods: ['GET'], name: 'program_show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig');
     }
 }
