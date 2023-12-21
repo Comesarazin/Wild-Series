da391e3e15c52a7ddb1863143404c62006f91ee1
ComeSarazin <sasacoco@orange.fr>
2023-12-21 10:57:11 +0100

    16 - Symfony : Validation

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -7,8 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
 
 #[ORM\Entity(repositoryClass: ProgramRepository::class)]
+#[UniqueEntity(fields: ['title'], message: 'Ce titre existe déjà')]
 class Program
 {
     #[ORM\Id]
@@ -16,10 +21,16 @@ class Program
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Ne me laisse pas tout vide')]
+    #[Assert\Length(
+    max: 255,
+    maxMessage: 'La catégorie saisie {{ value }} est trop longue, elle ne devrait pas dépasser {{ limit }} caractères',
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: 'Le synopsis ne doit pas être vide')]
     private ?string $synopsis = null;
 
     #[ORM\Column(length: 255, nullable: true)]
